888031c5f64754234ce6729f5facca95fc8a70fb
zhaoyingchuan <18146612665@163.com>
2020-01-24 15:19:27 +0800

    删除sample.py

--- sample.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-import arcpy
-
-aprx  = arcpy.mp.ArcGISProject(r"D:\Desktop\shiyan.mxd")
-m = arcpy.mp.ListDataFrames(mxd)[0]
-lyr = arcpy.mp.ListLayers(mxd, "", df)[0]
-lyr.definitionQuery = '"序号" = ' + "'1'"
-df.extent = lyr.getSelectedExtent()
-arcpy.RefreshActiveView()
